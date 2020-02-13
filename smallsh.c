be1f652d541e9bb9c9dc7fa39311a434ad21546b
David Passaro <passarod@os1.engr.oregonstate.edu>
2020-02-12 22:46:33 -0800

    input and cd x working

--- smallsh.c
+++ smallsh.c
@@ -9,6 +9,7 @@ int prompt();
 
 int main(int argc, char *argv[]){
   prompt();
+  return 0;
 }
 
 int prompt(){
@@ -20,6 +21,8 @@ int prompt(){
   int i,j,k;
   int cmdsLen=1;
   char *cmds[512];
+  char cwd[100];
+
   getBuf = (char *)malloc(buflen * sizeof(char));
 
   for( i=0;i<512;i++){
@@ -35,13 +38,7 @@ int prompt(){
         perror("Allocation of buffer failed");
         exit(1);
     }
-    //write(1,numChrs, 3);fflush(stdout);
-    //write(1," characters were read.\n",24);fflush(stdout);
-    //write(1,"You typed: ",11);fflush(stdout);
-    //printf("%s\n",getBuf);fflush(stdout);//REMOVE PRINTF
-    //printf("chr= %c\n",getBuf[0]);fflush(stdout);//REMOVE PRINTF
-    //write(1,&getBuf,numChrs);fflush(stdout);
-    //write(1,"\n",1);
+  
     write(1,": ", 1);fflush(stdout);
     numChrs = getline(&getBuf,&buflen,stdin);
     getBuf[strcspn(getBuf, "\n")] = '\0';
@@ -49,28 +46,16 @@ int prompt(){
     char* position = strchr(getBuf, ' ');
     char* tok;
     char c[2] = " ";
-    if (getBuf[0]=='e' && getBuf[1]=='c' && getBuf[2]=='h' && getBuf[3]=='o'){
+    //printf("%s getline\n",getBuf);fflush(stdout);
+    /*if (getBuf[0]=='e' && getBuf[1]=='c' && getBuf[2]=='h' && getBuf[3]=='o'){
       printf("echo!\n");fflush(stdout);
       }
     //check for echo cmd and comments first
-    else if (getBuf[0]=='#'){
+    else */if (getBuf[0]=='#'){
       printf("skipped\n");fflush(stdout);
       continue;
     }
-    else if(position != NULL ){//separate words (found space in str)
-      //*position = '\0';//change to end first word
-      //multiWords=1;
-      tok = strtok(getBuf,c);
-      i=0;
-      while(tok !=NULL){//put five strings in max
-        //printf(" %s toc\n",tok);
-        strcpy(cmds[i],tok);
-        printf(" %s \n",cmds[i]);
-        tok= strtok(NULL, c);
-        cmdsLen++;
-        i++;
-      }
-    }
+
     else if(( strcmp(getBuf,"exit")==0)){//check for exit
       //printf("exiting");fflush(stdout);
       free(getBuf);  
@@ -84,17 +69,47 @@ int prompt(){
     else if( strcmp(getBuf,"cd")==0){//check for cd
       printf("cding");fflush(stdout);
       chdir(getenv("HOME"));//go home
-      char cwd[100];
       if(getcwd(cwd,sizeof(cwd)) != NULL){
-      //printf("cwd= %s\n",cwd);fflush(stdout);
+      printf("cwd= %s\n",cwd);fflush(stdout);
       }
     }
-    else if(multiWords==1 && strcmp(getBuf,"cd")==0){
-      //chdir(getenv());//go to new dir
+    else if (strcmp(getBuf,"status")==0){
+
     }
-     
+    else if(position != NULL ){//separate words (found space in str)
+      //*position = '\0';//change to end first word
+      //multiWords=1;
+      //printf("abt to strtoc");fflush(stdout);
+      tok = strtok(getBuf,c);
+      i=0;
+      while(tok !=NULL){//put five strings in max
+       // printf(" %s toc\n",tok);fflush(stdout);
+        strcpy(cmds[i],tok);
+        //printf(" %s \n",cmds[i]);fflush(stdout);
+        tok= strtok(NULL, c);
+        cmdsLen++;
+        i++;
+      }
+    }
+    if( strcmp(cmds[0] ,"cd")==0){//more commands after
+     // printf("abtchdir  %s",cmds[1]);fflush(stdout);
+      if(chdir(cmds[1])!=0){perror("chdir failed");}//go to new dir     
+      if(getcwd(cwd,sizeof(cwd)) != NULL){
+        printf("cwd= %s\n",cwd);fflush(stdout);
+      }
+    }
 
   }//while 1 outer
   free(getBuf);
   return 0;
 }//main
+
+
+
+  //write(1,numChrs, 3);fflush(stdout);
+    //write(1," characters were read.\n",24);fflush(stdout);
+    //write(1,"You typed: ",11);fflush(stdout);
+    //printf("%s\n",getBuf);fflush(stdout);//REMOVE PRINTF
+    //printf("chr= %c\n",getBuf[0]);fflush(stdout);//REMOVE PRINTF
+    //write(1,&getBuf,numChrs);fflush(stdout);
+    //write(1,"\n",1);
