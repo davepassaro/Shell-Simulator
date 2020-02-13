b5f15d0ce1ec97f2d1e8ebae305a1220a46ef22f
David Passaro <passarod@os1.engr.oregonstate.edu>
2020-02-12 19:32:36 -0800

    basics done apart from status

--- smallsh.c
+++ smallsh.c
@@ -5,29 +5,31 @@
 #include <signal.h>
 #include <unistd.h>
   
-int cmds();
+int prompt();
 
 int main(int argc, char *argv[]){
-  cmds();
+  prompt();
 }
 
-int cmds(){
+int prompt(){
   char *getBuf;
   size_t buflen = 60;
   size_t chars;
   int numChrs;
-  int multwords=0; 
+  int multiWords=0; 
   int i,j,k;
-  char cmd1[200];
-  char cmd2[200];
-  char cmd3[200];
-  char cmd4[200];
-  char cmd5[200];
-  char cmd6[200];
+  int cmdsLen=1;
+  char *cmds[512];
+  getBuf = (char *)malloc(buflen * sizeof(char));
 
+  for( i=0;i<512;i++){
+    cmds[i]= (char *)malloc(30 * sizeof(char));
+    memset(cmds[i],'\0',30);
+    if(cmds[i] ==NULL){
+      perror("Alloc of cmds array failed");
+    }
+  }
   while(1){
-    multword=0;
-    getBuf = (char *)malloc(buflen * sizeof(char));
     if( getBuf == NULL)
     {
         perror("Allocation of buffer failed");
@@ -43,43 +45,52 @@ int cmds(){
     write(1,": ", 1);fflush(stdout);
     numChrs = getline(&getBuf,&buflen,stdin);
     getBuf[strcspn(getBuf, "\n")] = '\0';
-    strcpy(cmd1,getBuf);
+    strcpy(cmds[0],getBuf);
     char* position = strchr(getBuf, ' ');
-    
-    if (getBuf[0]=='e' && getBuf[0]=='c' && getBuf[0]=='h' && getBuf[0]=='o'){printf("echo";fflush(stdout);}
+    char* tok;
+    char c[2] = " ";
+    if (getBuf[0]=='e' && getBuf[1]=='c' && getBuf[2]=='h' && getBuf[3]=='o'){
+      printf("echo!\n");fflush(stdout);
+      }
     //check for echo cmd and comments first
     else if (getBuf[0]=='#'){
-     // printf("skipped\n");fflush(stdout);
+      printf("skipped\n");fflush(stdout);
       continue;
     }
-    else if(position != NULL && getBuf){//separate words (found space in str)
-      *position = '\0';//change to end first word
-      multword=1;
-      strcpy(cmd1,getBuf);//put first word here
-      *position = ' ';//change back to space
-      for (i=0;i<strlen(getBuf);i++){//at location after space put that word in
-        if (getBuf[i]== ' '){//first word passed
-          k=0
-          for ( j=(1+i);j<strlen(getBuf);j++){
-            cmd2[k]=getBuf[j];
-            k++;
-          }//more than one word in input
-
-    
-    else if(strcmp(getBuf,"exit")==0){//check for exit
+    else if(position != NULL ){//separate words (found space in str)
+      //*position = '\0';//change to end first word
+      //multiWords=1;
+      tok = strtok(getBuf,c);
+      i=0;
+      while(tok !=NULL){//put five strings in max
+        //printf(" %s toc\n",tok);
+        strcpy(cmds[i],tok);
+        printf(" %s \n",cmds[i]);
+        tok= strtok(NULL, c);
+        cmdsLen++;
+        i++;
+      }
+    }
+    else if(( strcmp(getBuf,"exit")==0)){//check for exit
       //printf("exiting");fflush(stdout);
       free(getBuf);  
+      for (i=0;i<512;i++){
+        free(cmds[i]);
+        cmds[i] =NULL;
+      }
+  
       return(0);//add in exit closing bg processes
     }
-    else if(multword==0 && strcmp(getBuf,"cd")==0){//check for cd
-     // printf("cding");fflush(stdout);
+    else if( strcmp(getBuf,"cd")==0){//check for cd
+      printf("cding");fflush(stdout);
       chdir(getenv("HOME"));//go home
       char cwd[100];
       if(getcwd(cwd,sizeof(cwd)) != NULL){
       //printf("cwd= %s\n",cwd);fflush(stdout);
       }
-    else if(multiword==1 && strcmp(getBuf,"cd")==0){
-      chdir(getenv());//go to new dir
+    }
+    else if(multiWords==1 && strcmp(getBuf,"cd")==0){
+      //chdir(getenv());//go to new dir
     }
      
 
